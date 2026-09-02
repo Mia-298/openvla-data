19d745c4fee1b8007807489785e7379d2367b3cd
Mia-298 <2982023833@qq.com>
2026-09-02 22:24:27 +0800

    auto push

--- test/numpy_learn.py
+++ test/numpy_learn.py
@@ -1,1 +1,11 @@
 import numpy as np
+# 实现基于 q01/q99 的归一化、裁剪和反归一化。
+# 归一化的动作
+action = np.array([
+    0.01, -0.02, 0.03,
+    0.1, -0.1, 0.2,
+    1.0
+])
+print(action.shape)
+print(action.dtype)
+
